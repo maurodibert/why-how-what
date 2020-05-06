4635bca583862cb7dd15557f14db6818ac3e806a
mau <maurodibert@gmail.com>
2020-05-06 06:38:38 -0300

    adding basic routing C

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-			</Switch>
-		</Router>
+		<div>
+			<Main />;
+		</div>
 	);
 }
 
